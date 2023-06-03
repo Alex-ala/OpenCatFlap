956253279cf4170736301a3d3d643e3e31806add
Alexander Landmesser <alexander@landmesser.online>
2023-06-03 15:42:47 +0200

    bla

--- src/OCFFlapControl.cpp
+++ src/OCFFlapControl.cpp
@@ -1,14 +1,60 @@
 #include <OCFFlapControl.h>
 
 namespace OCFFlapControl {
-    enum Direction { IN, OUT };
-    enum State { LOCKED, UNLOCKED };
-    Servo servo_in;
-    Servo servo_out;
+    void init(){
+        servo_in.attach(OCF_SERVO_IN_PIN);
+        servo_out.attach(OCF_SERVO_OUT_PIN);
+    }
 
-    void moveServo(Direction direction, int angle);
-    void setLockState(Direction direction, State state);
-    bool detectMotion(int timeout);
+    void moveServo(Direction direction, int angle){
+        switch(direction){
+            case Direction::IN:
+                servo_in.write(angle);
+                break;
+            case Direction::OUT:
+                servo_out.write(angle);
+        }
+    }
+    void setLockState(Direction direction, State state){
+        if(direction == Direction::IN){
+            flapState.last_change_in = esp_timer_get_time();
+            flapState.state_lock_in = state;
+            if(state == State::UNLOCKED){
+                moveServo(direction, OCF_SERVO_OUT_UNLOCKED);
+            }else{
+                moveServo(direction, OCF_SERVO_OUT_LOCKED);
+            }
+        }else{
+            flapState.last_change_out = esp_timer_get_time();
+            flapState.state_lock_out = state;
+            if(state == State::UNLOCKED){
+                moveServo(direction, OCF_SERVO_OUT_UNLOCKED);
+            }else{
+                moveServo(direction, OCF_SERVO_OUT_LOCKED);
+            } 
+        }
+    }
+    Direction detectMotion(int timeout){
+        delayMicroseconds(timeout);
+        return Direction::IN;
+    }
     
-    void loop(void* parameter);
+    void loop(void* parameter){
+        while(true){
+            Direction d = detectMotion(100);
+            if(d == Direction::NONE){
+                if(esp_timer_get_time() > (flapState.last_change_in + 2000) && 
+                   flapState.state_lock_in == State::UNLOCKED){
+                    setLockState(Direction::IN, State::LOCKED);
+                }               
+                if(esp_timer_get_time() > (flapState.last_change_out + 2000) && 
+                   flapState.state_lock_out == State::UNLOCKED){
+                    setLockState(Direction::OUT, State::LOCKED);
+                }
+                continue;
+            }
+            setLockState(d, State::UNLOCKED);
+
+        }
+    }
 }
--- src/OCFFlapControl.h
+++ src/OCFFlapControl.h
@@ -6,12 +6,24 @@
 #include <definitions.h>
 
 namespace OCFFlapControl {
-    enum Direction { IN, OUT };
+    enum Direction { IN, OUT, NONE };
     enum State { LOCKED, UNLOCKED };
+    struct FlapState {
+        State state_lock_in;
+        State state_lock_out;
+        bool allow_out;
+        bool allow_in;
+        uint64_t last_activity;
+        uint64_t last_change_in;
+        uint64_t last_change_out;
+    };
     Servo servo_in;
     Servo servo_out;
+    FlapState flapState;
 
+    void init();
     void moveServo(Direction direction, int angle);
     void setLockState(Direction direction, State state);
+    Direction detectMotion(int timeout);
 };
 #endif // __OCFFLAPCONTROL_H__
